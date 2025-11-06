bbbada4a107b0901091810bccdef4dabf1050e7d
Rahul Kumar <rkmandal9105@gmail.com>
2025-11-06 20:03:46 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+
+
+
+const heading = React.createElement("h1", {} ,"Hello World!")
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+
+console.log(2-0);
